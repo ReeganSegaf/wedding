694cf6d26abc084b3bb899c1a85a5029b9886459
Kitsune2373 <aqilassegaf0@gmail.com>
2026-04-14 15:09:42 +0700

    update wedding site

--- script.js
+++ script.js
@@ -17,7 +17,7 @@
 /* ============================================================
    CONSTANTS
 ============================================================ */
-const WEDDING_DATE = new Date('2026-06-06T08:00:00'); // 06 Juni 2026, 08:00 WIB
+const WEDDING_DATE = new Date('2026-06-07T08:00:00'); // 06 Juni 2026, 08:00 WIB
 const SCRATCH_THRESHOLD = 0.90;   // 90% cleared triggers confetti
 
 
